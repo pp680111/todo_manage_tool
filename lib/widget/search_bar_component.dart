10ae55892bc427f77bdcfedf9f99101357db7617
DESKTOP-9CAQJI2\zst <zhangst5784@gmail.com>
2024-11-01 00:12:17 +0800

    feat: 搜索栏样式调整

--- lib/widget/search_bar_component.dart
+++ lib/widget/search_bar_component.dart
@@ -7,35 +7,22 @@ class SearchBarComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(children: [
-      // Container(
-      //   padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      //   child: SearchAnchor(
-      //     builder: (BuildContext context, SearchController controller) {
-      //       return SearchBar(
-      //         controller: controller,
-      //         onTap: () {
-      //           controller.openView();
-      //         },
-      //         onChanged: (_) {
-      //           controller.openView();
-      //         },
-      //         leading: const Icon(Icons.search),
-      //       );
-      //     },
-      //     viewElevation: false,
-      //     suggestionsBuilder: (BuildContext context, SearchController controller) {
-      //       return [];
-      //     }
-      //   ),
-      // )
       Expanded(
         child: Container(
-          padding: const EdgeInsets.only(top: 16),
-          height: 60,
+          padding: const EdgeInsets.only(top: 8),
+          height: 50,
           child: CupertinoSearchTextField()
         )
       ),
-      Icon(Icons.add)
+      Container(
+        padding: const EdgeInsets.only(top: 10),
+        child: IconButton(
+          icon: const Icon(Icons.add, size: 40,),
+          onPressed: () {
+
+          },
+        )
+      )
     ]);
   }
 }
