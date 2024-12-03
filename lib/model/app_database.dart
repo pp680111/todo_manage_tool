af0da6c73ddec3e9e929d3a1a4cc3d9e1d4de23b
zhangshutao <zhangshutao@ava.com.cn>
2024-12-03 19:45:03 +0800

    feat: 添加针对release和debug模式使用不同的db文件的设计

--- lib/model/app_database.dart
+++ lib/model/app_database.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:todo_manage/model/todo_thing/todo_thing.dart';
 import 'package:todo_manage/model/todo_thing/todo_thing_dao.dart';
@@ -22,7 +23,17 @@ class AppDatabase extends _$AppDatabase {
   static QueryExecutor _openConnection() {
     return LazyDatabase(() async {
       final dbFolder = await getApplicationDocumentsDirectory();
-      File dbFile = File('${dbFolder.path}/app_database.sqlite');
+
+      String path = "";
+      if (kDebugMode) {
+        path = '${dbFolder.path}/debug/app_database.sqlite';
+      } else if (kReleaseMode) {
+        path = '${dbFolder.path}/release/app_database.sqlite';
+      } else {
+        throw Exception("Unknown build mode");
+      }
+
+      File dbFile = File(path);
 
       return NativeDatabase(dbFile, logStatements: true);
     });
