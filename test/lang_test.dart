388b63262b1cbc93adcc45f447435f57b22f3e4b
DESKTOP-9CAQJI2\zst <zhangst5784@gmail.com>
2024-12-06 00:02:21 +0800

    fix: 补充遗漏的提交

--- test/lang_test.dart
+++ test/lang_test.dart
@@ -1,31 +1,30 @@
-
-import 'dart:ffi';
-import 'dart:io';
-
-import 'package:drift/native.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:sqlite3/open.dart';
-import 'package:todo_manage/model/category/category.dart';
-import 'package:todo_manage/model/category/category_db.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  open.overrideFor(OperatingSystem.windows, _openOnWindows);
-
-  final CategoryDb db = CategoryDb.forTest(NativeDatabase.memory());
-
-  await db.into(db.category)
-      .insert(CategoryCompanion.insert(name: 'zst',
-        createTime: DateTime.now(),
-      updateTime: DateTime.now()));
-  List<CategoryData> list = await db.select(db.category).get();
-  print('$list');
-
-}
-
-DynamicLibrary _openOnWindows() {
-  final script = File(Platform.script.toFilePath()).parent;
-  final libraryNextToScript = File('${script.path}\\sqlite3.dll');
-  return DynamicLibrary.open(libraryNextToScript.path);
-}
+//
+// import 'dart:ffi';
+// import 'dart:io';
+//
+// import 'package:drift/native.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:sqlite3/open.dart';
+// import 'package:todo_manage/model/category/category_db.dart';
+//
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//
+//   open.overrideFor(OperatingSystem.windows, _openOnWindows);
+//
+//   final CategoryDb db = CategoryDb.forTest(NativeDatabase.memory());
+//
+//   await db.into(db.category)
+//       .insert(CategoryCompanion.insert(name: 'zst',
+//         createTime: DateTime.now(),
+//       updateTime: DateTime.now()));
+//   List<CategoryData> list = await db.select(db.category).get();
+//   print('$list');
+//
+// }
+//
+// DynamicLibrary _openOnWindows() {
+//   final script = File(Platform.script.toFilePath()).parent;
+//   final libraryNextToScript = File('${script.path}\\sqlite3.dll');
+//   return DynamicLibrary.open(libraryNextToScript.path);
+// }
