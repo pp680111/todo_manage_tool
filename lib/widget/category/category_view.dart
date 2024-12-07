1dfdb0fbc5d8fd8435898f0c61dec346eeb9d857
zhangshutao <zhangshutao@ava.com.cn>
2024-12-07 14:10:48 +0800

    feat: 提交下未完成的代码

---
+++ lib/widget/category/category_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class CategoryView extends StatefulWidget {
+  const CategoryView({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CategoryViewState();
+  }
+}
+
+class _CategoryViewState extends State<CategoryView> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
--- lib/widget/search_bar_component.dart
+++ lib/widget/search_bar_component.dart
@@ -4,8 +4,14 @@ import 'package:flutter/material.dart';
 class SearchBarComponent extends StatefulWidget {
   Function onSearchChange;
   Function onAddButtonPress;
+  bool displayBackwardButton;
+  void Function(BuildContext ctx)? onBackwardButtonPress;
 
-  SearchBarComponent({super.key, required this.onSearchChange, required this.onAddButtonPress});
+  SearchBarComponent({super.key,
+    required this.onSearchChange,
+    required this.onAddButtonPress,
+    this.displayBackwardButton = true,
+    this.onBackwardButtonPress});
 
   @override
   State<StatefulWidget> createState() {
@@ -16,28 +22,42 @@ class SearchBarComponent extends StatefulWidget {
 class _SearchBarComponentState extends State<SearchBarComponent> {
   @override
   Widget build(BuildContext context) {
-    return Row(children: [
-      Expanded(
-          child: Container(
-              padding: const EdgeInsets.only(top: 8),
-              height: 50,
-              child: CupertinoSearchTextField(
-                placeholder: "请输入关键词",
-                onChanged: (text) {
-                  widget.onSearchChange(text);
-                },
-              )
-          )
-      ),
-      Container(
-          padding: const EdgeInsets.only(top: 10),
-          child: IconButton(
-            icon: const Icon(Icons.add, size: 40,),
-            onPressed: () {
-              widget.onAddButtonPress(context);
-            },
-          )
-      )
-    ]);
+    return Row(
+      children: [
+        if (widget.displayBackwardButton)
+          Container(
+            padding: const EdgeInsets.only(top: 10),
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back_rounded, size: 36),
+              onPressed: () {
+                if (widget.onBackwardButtonPress != null) {
+                  widget.onBackwardButtonPress!(context);
+                }
+              },
+            ),
+          ),
+        Expanded(
+            child: Container(
+                padding: const EdgeInsets.only(top: 8),
+                height: 50,
+                child: CupertinoSearchTextField(
+                  placeholder: "请输入关键词",
+                  onChanged: (text) {
+                    widget.onSearchChange(text);
+                  },
+                )
+            )
+        ),
+        Container(
+            padding: const EdgeInsets.only(top: 10),
+            child: IconButton(
+              icon: const Icon(Icons.add, size: 40,),
+              onPressed: () {
+                widget.onAddButtonPress(context);
+              },
+            )
+        )
+      ]
+    );
   }
 }
