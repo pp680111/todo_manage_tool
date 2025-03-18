5c41eacaae8f9b7d20b3e0264b713e5f1d5198ce
zhangshutao <zhangshutao@ava.com.cn>
2025-03-18 14:40:14 +0800

    feat: 添加logo

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
         useMaterial3: true,
+
       ),
       home: const MainPage()
     );
